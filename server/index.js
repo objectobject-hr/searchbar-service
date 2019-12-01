f541e64dcebb9544a1860864b2262a62320a905b
Steven Contreras <SFContra@gmail.com>
2019-11-30 22:19:56 -0800

    build and connect server to serve html

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const parser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.listen(PORT, () => {
+  console.log(`server is listening on post apocalyptic ${PORT}`);
+});
