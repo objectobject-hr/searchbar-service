0c78869d5b52265323a21726dbf89cf9563bc547
Steven Contreras <SFContra@gmail.com>
2019-12-09 14:05:30 -0800

    complete css of first div of the header

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -10,12 +10,51 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div class="searchbar-div">
           <header class="header" role="banner">
-            This is React!
+            <div class="searchbar-div" class="headermessages"></div>
+            <div class="searchbar-div" class="header-top-menu-full">
+              <ul class="header-top-menu-content">
+                <li class="header-top-menu-items">
+                  <a class="searchbar-a" class="header-top-menu-link">
+                    <span class="header-top-menu-item">Customer Service</span>
+                  </a>
+                </li>
+                <li class="header-top-menu-items">
+                  <a class="header-top-menu-link">
+                    <span class="header-top-menu-item">IKEA Family</span>
+                  </a>
+                </li>
+                <li class="header-top-menu-items">
+                  <a class="header-top-menu-link">
+                    <span class="header-top-menu-item">IKEA For Business</span>
+                  </a>
+                </li>
+                <li class="header-top-menu-items">
+                  <a class="header-top-menu-link">
+                    <span class="header-top-menu-item">IKEA planning Tools</span>
+                  </a>
+                </li>
+                <li class="header-top-menu-items">
+                  <a class="header-top-menu-link">
+                    <span class="header-top-menu-item">Track My Order</span>
+                  </a>
+                </li>
+                <li class="header-top-menu-items">
+                  <a class="header-top-menu-link">
+                    <span class="header-top-menu-item">Find a Location</span>
+                  </a>
+                </li>
+                <li class="header-top-menu-items">
+                  <a class="header-top-menu-link">
+                    <span class="header-top-menu-item">Credit Card</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </header>
-          <div data-namespace="search-box-overlay"></div>
-          <div data-namespace="search-box">
+          <div class="searchbar-div" data-namespace="search-box-overlay"></div>
+          <div class="searchbar-div" data-namespace="search-box">
             
           </div>
       </div>
