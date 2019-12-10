d20ceaf7731168bec121c9abf219b6bb1c86cd53
Steven Contreras <SFContra@gmail.com>
2019-12-10 09:56:37 -0800

    readjusted the arriba

--- client/src/components/arriba.jsx
+++ client/src/components/arriba.jsx
@@ -1,45 +1,45 @@
 import React from 'react';
 
 const Arriba = (props) => (
-    <div class="searchbar-div" class="header-top-menu-full">
-    <ul class="header-top-menu-content">
-      <li class="header-top-menu-items">
-        <a class="searchbar-a" class="header-top-menu-link">
-          <span class="header-top-menu-item">Customer Service</span>
-        </a>
-      </li>
-      <li class="header-top-menu-items">
-        <a class="header-top-menu-link">
-          <span class="header-top-menu-item">IKEA Family</span>
-        </a>
-      </li>
-      <li class="header-top-menu-items">
-        <a class="header-top-menu-link">
-          <span class="header-top-menu-item">IKEA For Business</span>
-        </a>
-      </li>
-      <li class="header-top-menu-items">
+    <div class="top-menu-d">
+      <ul class="top-menu__contend-d">
+        <li class="top-menu__item">
+          <a class="top-menu__link-d">
+            <span class="top-menu__item header__h6">Customer Service</span>
+          </a>
+        </li>
+        <li class="header-top-menu-items">
+          <a class="header-top-menu-link">
+            <span class="header-top-menu-item">IKEA Family</span>
+          </a>
+        </li>
+        <li class="header-top-menu-items">
+          <a class="header-top-menu-link">
+            <span class="header-top-menu-item">IKEA For Business</span>
+          </a>
+        </li>
+        <li class="header-top-menu-items">
         <a class="header-top-menu-link">
           <span class="header-top-menu-item">IKEA planning Tools</span>
         </a>
-      </li>
-      <li class="header-top-menu-items">
-        <a class="header-top-menu-link">
-          <span class="header-top-menu-item">Track My Order</span>
-        </a>
-      </li>
-      <li class="header-top-menu-items">
-        <a class="header-top-menu-link">
-          <span class="header-top-menu-item">Find a Location</span>
-        </a>
-      </li>
-      <li class="header-top-menu-items">
-        <a class="header-top-menu-link">
-          <span class="header-top-menu-item">Credit Card</span>
-        </a>
-      </li>
-    </ul>
-  </div>
+        </li>
+        <li class="header-top-menu-items">
+          <a class="header-top-menu-link">
+            <span class="header-top-menu-item">Track My Order</span>
+          </a>
+        </li>
+        <li class="header-top-menu-items">
+          <a class="header-top-menu-link">
+            <span class="header-top-menu-item">Find a Location</span>
+          </a>
+        </li>
+        <li class="header-top-menu-items">
+          <a class="header-top-menu-link">
+            <span class="header-top-menu-item">Credit Card</span>
+          </a>
+        </li>
+      </ul>
+    </div>
 )
 
 export default Arriba;
--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -4,9 +4,14 @@ const SearchBar = (props) => (
   <div data-namespace="search-box" data-version="v1.130.0">
     <form role="search" action="https://www.ikea.com/us/en/search/" class="search-box">
       <div class="search-field">
-        <input type="search" name="q" spellCheck="false" aria-label="Search by product" aria-placeholder="Search by product" autoCapitalize="off" autoComplete="off" autoCorrect="off"></input>    
-        <span >
-          </span>    
+       ::before
+        <input type="search" name="q" class="search-field__input" spellCheck="false" aria-label="Search by product" aria-placeholder="Search by product" autoCapitalize="off" autoComplete="off" autoCorrect="off"></input>    
+        <span class="search-suggestions">
+          "Search for"
+          <ul class="search-suggestions">
+            <li aria-hidden="false">categories</li>
+          </ul>
+        </span>    
       </div>
     </form>
   </div>  
@@ -14,14 +19,11 @@ const SearchBar = (props) => (
 
 export default SearchBar; 
 
-/*   ::before
+/*  
 class="searchbar-div"
-class="search-field__input"
+
 class="search-suggestions__item"
-class="search-suggetions__list"
- class="search-suggestions"
- <li aria-hidden="false">categories</li>
-              <li aria-hidden="false">content</li>
-                <li aria-hidden="true">products</li>
-                          <ul ></ul>    
+ 
+<li aria-hidden="true">content</li>
+            <li aria-hidden="true">products</li>
 */
