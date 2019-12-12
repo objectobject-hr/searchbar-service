58299c4bde16d8c6c03863aff1f7de1debe5a592
Steven Contreras <SFContra@gmail.com>
2019-12-12 15:37:17 -0800

    implment searchbar type

--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -5,16 +5,12 @@ const SearchBar = ({flasher, searchText}) => (
     <form role="search" action="https://www.ikea.com/us/en/search/" class="search-box">
       <div class="search-field">
         <span class="hnf-btn__icon-bg"></span>
-        <input type="search" name="q" class="search-field__input" spellCheck="false" aria-label="Search by product" aria-placeholder="Search by product" autoCapitalize="off" autoComplete="off" autoCorrect="off"></input>    
+        <input type="search" value={searchText} name="q" class="search-field__input" spellCheck="false" aria-label="Search by product" aria-placeholder="Search by product" autoCapitalize="off" autoComplete="off" autoCorrect="off"></input>    
         <span class="search-suggestions">
           <span class="hnf-btn__copy searchbar-padder">
             <svg class="hnf-svg-icon" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" >
               <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
             </svg>
-          </span>
-          <span type="text" value={searchText}
-          onChange={alert(searchText)}
-          >
           </span>
         </span>    
       </div>
