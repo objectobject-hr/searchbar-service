75d90d1c883cc5b7289940d9d8d43d6c0b2ed180
Steven Contreras <SFContra@gmail.com>
2019-12-13 14:28:20 -0800

    set up modal but not triggerable

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import Cabeza from './cabeza.jsx';
 import SearchBar from './searchBar.jsx';
+import PropTypes from 'prop-types';
 import '../style.css';
 
 class App extends React.Component {
--- client/src/components/searchDrop.jsx
+++ client/src/components/searchDrop.jsx
@@ -1,24 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 var pops = ["desk", "dresser", "tv stand", "mirror", "shelves", "kallax"];
 
-const SearchDrop = () => (
-  <div className="sb-dropdown">
-    <div className="sb-search-results">
-      <div className="sb-search-results__container">
-        <h2 className="sb-search-results__heading">Popular searches</h2>
-        <ol className="dd-sb-search-results__list">
-          {pops.map((item, i) => (
-            <li className="dd-sb-search-results__list" key={i}>
-              <a className="dd-sb-search-results-item__link sb-a" href={`https://www.ikea.com/us/en/search/?q=${item}`}>
-                {item}
-              </a>
-            </li>
-          ))}
-        </ol>
+class SearchDrop extends React.Component {
+  render() {
+    if(!this.props.show) {
+      return null;
+    }
+
+    const backdropStyle = {
+      position: 'fixed',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'rgba(0,0,0,0.3)',
+      padding: 50
+    };
+
+    const modalStyle = {
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      maxWidth: 500,
+      minHeight: 300,
+      margin: '0 auto',
+      padding: 30
+    };
+
+    return (
+      <div className="sb-dropdown backdrop" style={{backdropStyle}}>
+        <div className="sb-search-results modal" style={{modalStyle}}>
+          {this.props.children}
+          <div className="sb-search-results__container">
+            <h2 className="sb-search-results__heading">Popular searches</h2>
+            <ol className="dd-sb-search-results__list">
+              {pops.map((item, i) => (
+                <li className="dd-sb-search-results__list" key={i}>
+                  <a className="dd-sb-search-results-item__link sb-a" href={`https://www.ikea.com/us/en/search/?q=${item}`}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ol>
+          </div>
+          <div className="footer">
+            <button onClick={this.props.onClose}>
+              Close
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    );
+  }
+}
+
+SearchDrop.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  show: PropTypes.bool,
+  children: PropTypes.node
+};
 
 export default SearchDrop;
