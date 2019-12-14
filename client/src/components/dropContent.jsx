064ee61c202d029829a8ece0d2666a1f34d53352
Steven Contreras <SFContra@gmail.com>
2019-12-14 11:44:38 -0800

    adjust 2nd column of searchbar type dropdown

--- client/src/components/dropContent.jsx
+++ client/src/components/dropContent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 var pops = ["desk", "dresser", "tv stand", "mirror", "shelves", "kallax"];
 var lex = ["lex", "lex drawer unit", "rmchair", "lex desk", "rea rug", "lgot"];
-var clum = ["rmchair", "lex desk", "rea rug", "lgot"];
+var clum = ["IKEA ART EVENT 2019 Limited Collection", "The art of organizing: toy storage", "How to make a DIY canvas for your artwork"];
 
 const DropContent = () => (
   <div className="sb-search-results">
@@ -19,24 +19,23 @@ const DropContent = () => (
     <ol className="dd-sb-search-results__list sb-search-results__container" style={{alignself: 'stretch', borderLeft: 'gainsboro 1px solid'}}>
       <li className="dd-sb-search-results-item--planner">
         <a className="dd-sb-pri-search-results-item__link-nd" href={`https://www.ikea.com/us/en/search/?q=alex`}>
-          <b>Algot Planner</b>
+          Algot Planner
         </a>
       </li>
       <li className="dd-sb-search-results-item--planner">
-        <a className="dd-sb-pri-search-results-item__link-nd" href={`https://www.ikea.com/us/en/search/?q=alex`}>
+        <a className="dd-sb-sec-search-results-item__link-nd" href={`https://www.ikea.com/us/en/search/?q=alex`}>
           <b>Alex drawer unit</b>
         </a>
       </li>
       <li className="dd-sb-search-results-item--planner">
-        <a className="dd-sb-pri-search-results-item__link-nd" href={`https://www.ikea.com/us/en/search/?q=alex`}>
+        <a className="dd-sb-sec-search-results-item__link-nd" href={`https://www.ikea.com/us/en/search/?q=alex`}>
           <b>Algot Planner</b>
         </a>
       </li>
       {clum.map((item, i) => (
-        <li style={{paddingBottom: '1.6rem', fontSize: 'large'}} key={i}>
+        <li style={{paddingBottom: '.5rem', fontSize: 'large'}} key={i}>
           <a className="dd-sb-search-results-item__link-nd" href={`https://www.ikea.com/us/en/search/?q=a${item}`}>
-            a
-            <b>{item}</b>
+            {item}
           </a>
         </li>
       ))}
